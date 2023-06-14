832aab50b45b2a6b8e72cd27b79fce0b193c031b
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-06-14 19:52:54 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,4 +4,31 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
-galleryEl.innerHTML = '';
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+            src="${image.preview}" 
+            data-source="${image.original}"
+            alt="${image.description}" 
+            class="gallery__image"
+          />
+        </a>
+      </li>`
+  )
+  .join("");
+galleryEl.insertAdjacentHTML("afterbegin", markup);
+
+const imgClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}"width="800" height="600">`);
+  instance.show();
+};
+
+list.addEventListener("click", imgClick);
