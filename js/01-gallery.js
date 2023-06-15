6c9731bba3a86daf2ac908c6d32957aa3fa9d8a6
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-06-15 20:11:37 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,6 +27,7 @@ const imgClick = (event) => {
   if (event.target.nodeName !== "IMG") {
     return;
   }
+
   const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}"width="800" height="600">`);
   instance.show();
